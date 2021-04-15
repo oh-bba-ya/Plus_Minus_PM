ccaaaebc82490f752c57d9ad60bfe25092805204
JuyeongHwang <49023736+JuyeongHwang@users.noreply.github.com>
2021-04-15 20:01:47 +0900

    loginScene

---
+++ PlusMinus_Project/Assets/Scripts/LoginScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginScript : MonoBehaviour
+{
+    public GameObject SignupCanvas;
+    public InputField Id;
+    public InputField Password;
+    public Button Login;
+    public Button Main_SignUp;
+
+    public Button Signup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SignupCanvas.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickMainSignUp()
+    {
+        SignupCanvas.SetActive(true);
+    }
+}
