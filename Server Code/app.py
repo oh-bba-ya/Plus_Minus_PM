8fa06a070629d30c1ed155157866ecbb4cbfe33e
DaLae37 <dalae37@gmail.com>
2021-04-26 20:15:23 +0900

    Add Socket.IO Module

--- Server Code/app.py
+++ Server Code/app.py
@@ -32,7 +32,7 @@ def login():
     if pmuser.password != password :
         return "Failed"
     else :
-        return "Success"
+        return {"result" : "success", "money", pmuser.money}
 
 if __name__ == "__main__":
     basedir = os.path.abspath(os.path.dirname(__file__))
