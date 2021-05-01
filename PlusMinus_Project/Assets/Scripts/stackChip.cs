e156f5171503bea530dc227047687d35e96c07e5
JuyeongHwang <49023736+JuyeongHwang@users.noreply.github.com>
2021-05-01 10:00:44 +0900

    짤랑 사운드 추가

--- PlusMinus_Project/Assets/Scripts/stackChip.cs
+++ PlusMinus_Project/Assets/Scripts/stackChip.cs
@@ -7,13 +7,14 @@ public class stackChip : MonoBehaviour
 {
     //GameObject chip1;
     public List<GameObject> Slot = new List<GameObject>();
-    public GameObject myObject;
     public List<GameObject> Chips = new List<GameObject>();
+    public AudioClip drop;
     public Button Check; public Button gameQuit;
     public Button gameRestart;
     int maxChips = 10;
 
     public GameObject AudioBg;
+    public GameObject ChipAudio;
     public GameObject winner;
     public GameObject loser;
     public GameObject draw;
@@ -44,21 +45,34 @@ public class stackChip : MonoBehaviour
     {
         if(maxChips > 0)
         {
+
+            AudioSource AS = ChipAudio.GetComponent<AudioSource>();
             int amount = Random.Range(0, 3);
             
             for(int i = 0; i < amount; i++)
             {
                 int slotI = Random.Range(0, Slot.Count);
                 int chipI = Random.Range(0, Chips.Count);
+                
                 GameObject myChip = Instantiate(Chips[chipI], Slot[slotI].transform.position, Quaternion.identity) as GameObject;
                 myChip.transform.SetParent(Slot[slotI].transform);
                 maxChips--;
 
             }
-            
-        }        
+            playSound(drop, AS);
+
+        }   
+        
     }
 
+    public void playSound(AudioClip clip, AudioSource audioPlayer)
+    {
+        audioPlayer.Stop();
+        audioPlayer.clip = clip;
+        audioPlayer.loop = false;
+        audioPlayer.time = 0;
+        audioPlayer.Play();
+    }
     public void OnClickCheckChip()
     {
         CreateGameObject();
